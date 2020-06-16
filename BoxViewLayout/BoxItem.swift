f62e4ec7b2d5e9eb28b6c186b34d2a02908ac636
Vladimir Dudkin <vdudkin@lanapro.com>
2020-06-17 00:36:16 +0300

    pod lint issues fixed

--- BoxViewLayout/BoxItem.swift
+++ BoxViewLayout/BoxItem.swift
@@ -20,68 +20,68 @@ public struct BoxItem {
     }
     
     // creates new BoxItem from existing, by setting left pin
-    public func boxLeft(_ leftPin: BoxLayout.Pin?) -> Self {
-        return Self.init(view: view, layout: layout.with(.left, leftPin))
+    public func boxLeft(_ leftPin: BoxLayout.Pin?) -> BoxItem {
+        return BoxItem(view: view, layout: layout.with(.left, leftPin))
     }
     
     // Creates new BoxItem from existing, by setting exact left padding.
     public func boxLeft(_ left: CGFloat?) -> BoxItem {
-        return Self.init(view: view, layout: layout.with(.left, ==left))
+        return BoxItem(view: view, layout: layout.with(.left, ==left))
     }
     
     // creates new BoxItem from existing, by setting right pin
-    public func boxRight(_ rightPin: BoxLayout.Pin?) -> Self {
-        return Self.init(view: view, layout: layout.with(.right, rightPin))
+    public func boxRight(_ rightPin: BoxLayout.Pin?) -> BoxItem {
+        return BoxItem(view: view, layout: layout.with(.right, rightPin))
     }
     
     // creates new BoxItem from existing, by setting exact right padding
-    public func boxRight(_ right: CGFloat?) -> Self {
-        return Self.init(view: view, layout: layout.with(.right, ==right))
+    public func boxRight(_ right: CGFloat?) -> BoxItem {
+        return BoxItem(view: view, layout: layout.with(.right, ==right))
     }
     
     // creates new BoxItem from existing, by setting left and right pins
     public func boxLeftRight(_ leftPin: BoxLayout.Pin?, _ rightPin: BoxLayout.Pin?) -> BoxItem {
-        return Self.init(view: view, layout: layout.with(.left, leftPin).with(.right, rightPin))
+        return BoxItem(view: view, layout: layout.with(.left, leftPin).with(.right, rightPin))
     }
     
     // creates new BoxItem from existing, by setting exact left and right paddings
     public func boxLeftRight(_ left: CGFloat?, _ right: CGFloat?) -> BoxItem {
-        return Self.init(view: view, layout: layout.with(.left, ==left).with(.right, ==right))
+        return BoxItem(view: view, layout: layout.with(.left, ==left).with(.right, ==right))
     }
     
     // creates new BoxItem from existing, by setting top pin
-    public func boxTop(_ topPin: BoxLayout.Pin?) -> Self {
-        return Self.init(view: view, layout: layout.with(.top, topPin))
+    public func boxTop(_ topPin: BoxLayout.Pin?) -> BoxItem {
+        return BoxItem(view: view, layout: layout.with(.top, topPin))
     }
     
     // creates new BoxItem from existing, by setting exact top padding
-    public func boxTop(_ top: CGFloat?) -> Self {
-        return Self.init(view: view, layout: layout.with(.top, ==top))
+    public func boxTop(_ top: CGFloat?) -> BoxItem {
+        return BoxItem(view: view, layout: layout.with(.top, ==top))
     }
     
     // creates new BoxItem from existing, by setting bottom pin
-    public func boxBottom(_ bottomPin: BoxLayout.Pin?) -> Self {
-        return Self.init(view: view, layout: layout.with(.bottom, bottomPin))
+    public func boxBottom(_ bottomPin: BoxLayout.Pin?) -> BoxItem {
+        return BoxItem(view: view, layout: layout.with(.bottom, bottomPin))
     }
     
     // creates new BoxItem from existing, by setting exact bottom padding
-    public func boxBottom(_ bottom: CGFloat?) -> Self {
-        return Self.init(view: view, layout: layout.with(.bottom, ==bottom))
+    public func boxBottom(_ bottom: CGFloat?) -> BoxItem {
+        return BoxItem(view: view, layout: layout.with(.bottom, ==bottom))
     }
     
     // creates new BoxItem from existing, by setting top and bottom pins
     public func boxTopBottom(_ topPin: BoxLayout.Pin?, _ bottomPin: BoxLayout.Pin?) -> BoxItem {
-        return Self.init(view: view, layout: layout.with(.top, topPin).with(.bottom, bottomPin))
+        return BoxItem(view: view, layout: layout.with(.top, topPin).with(.bottom, bottomPin))
     }
     
     // creates new BoxItem from existing, by setting exact top and bottom paddings
     public func boxTopBottom(_ top: CGFloat?, _ bottom: CGFloat?) -> BoxItem {
-        return Self.init(view: view, layout: layout.with(.top, ==top).with(.bottom, ==bottom))
+        return BoxItem(view: view, layout: layout.with(.top, ==top).with(.bottom, ==bottom))
     }
     
     // creates new BoxItem from existing, by adding alignment along X-axis
     public func boxCenterX(offset: CGFloat = 0.0, padding: CGFloat? = 0.0) -> BoxItem {
-        let alignedItem =  Self.init(view: view, layout: layout.with(.centerX, ==offset))
+        let alignedItem =  BoxItem(view: view, layout: layout.with(.centerX, ==offset))
         if let padding = padding {
             let paddingPin = >=padding
             return alignedItem.boxLeftRight(paddingPin, paddingPin)
@@ -94,7 +94,7 @@ public struct BoxItem {
     
     // creates new BoxItem from existing, by adding alignment along Y-axis
     public func boxCenterY(offset: CGFloat = 0.0, padding: CGFloat? = 0.0) -> BoxItem {
-        let alignedItem =  Self.init(view: view, layout: layout.with(.centerY, ==offset))
+        let alignedItem =  BoxItem(view: view, layout: layout.with(.centerY, ==offset))
         if let padding = padding {
             let paddingPin = >=padding
             return alignedItem.boxTopBottom(paddingPin, paddingPin)
@@ -207,7 +207,7 @@ extension UIView {
     }
     
     public func boxAll(_ padding: CGFloat) -> BoxItem {
-        return boxInsets(.all(padding))
+        return boxInsets(UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding))
     }
     
     @discardableResult
@@ -229,17 +229,17 @@ extension Array where Element: UIView {
     
     public func boxXY(_ x: BoxLayout.Pin.Pair, _ y: BoxLayout.Pin.Pair) -> [BoxItem] {
         let layout = BoxLayout.pairs(x: x, y: y)
-        return self.map{BoxItem(view: $0, layout: layout)}
+        return map{BoxItem(view: $0, layout: layout)}
     }
     
     // Creates BoxItems array from array of views using specified layout for all views.
     public func boxLayout(_ layout: BoxLayout) -> [BoxItem] {
-        return self.map{BoxItem(view: $0, layout: layout)}
+        return map{BoxItem(view: $0, layout: layout)}
     }
     
     // Creates BoxItems array from array of views using same "zero" layout (zero constant constraints to all four edges) for all views.
     public var boxZero: [BoxItem] {
-        return self.map{BoxItem(view: $0, layout: .zero)}
+        return map{BoxItem(view: $0, layout: .zero)}
     }
     
     
