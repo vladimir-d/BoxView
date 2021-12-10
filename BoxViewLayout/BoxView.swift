1546a2840b1478862a8d6d6adb3b0df35426dfe1
Vladimir Dudkin <vdudkin@lanapro.com>
2021-12-10 20:45:32 +0200

    BoxView code refactored: removing and adding object code grouped into internal methods, Managing z-order updated

--- BoxViewLayout/BoxView.swift
+++ BoxViewLayout/BoxView.swift
@@ -12,7 +12,7 @@ import UIKit
 open class BoxView: UIView {
     
 // MARK: - Public -
-    
+
     // MARK: -- Inits
     
     public init(axis: BoxLayout.Axis = .y, spacing: CGFloat = 0.0, insets: UIEdgeInsets = .zero) {
@@ -44,7 +44,7 @@ open class BoxView: UIView {
     // X is horizontal and Y is vertical.
     public var axis: BoxLayout.Axis = .y {
         didSet {
-            setNeedsUpdateConstraints()
+            setNeedsUpdateAllConstraints()
         }
     }
     
@@ -71,7 +71,20 @@ open class BoxView: UIView {
     @IBInspectable
     public var spacing: CGFloat = 0.0 {
         didSet {
-            setNeedsUpdateConstraints()
+            setNeedsUpdateAllConstraints()
+        }
+    }
+    
+    //
+    public var frontViews: [UIView]? {
+        didSet {
+            frontViews?.forEach { bringSubviewToFront($0) }
+        }
+    }
+    
+    public var backViews: [UIView]? {
+        didSet {
+            backViews?.forEach { sendSubviewToBack($0) }
         }
     }
     
@@ -85,7 +98,7 @@ open class BoxView: UIView {
                 else {
                     observers = [Int: NSKeyValueObservation]()
                 }
-                setNeedsUpdateConstraints()
+                setNeedsUpdateAllConstraints()
             }
         }
     }
@@ -105,14 +118,14 @@ open class BoxView: UIView {
     // MARK: -- readonly vars
     
     // array of all automatically created by BoxView constraints
-    public private(set) var managedConstraints = [NSLayoutConstraint]()
+    public internal(set) var managedConstraints = [NSLayoutConstraint]()
     
     // array of views of items.
     // it is a subset of boxView.subviews
-    public private(set) var managedViews = [UIView]()
+    public internal(set) var managedViews = [UIView]()
     
     // array of guides of items.
-    public private(set) var managedGuides = [UILayoutGuide]()
+    public internal(set) var managedGuides = [UILayoutGuide]()
     
     
     // MARK: -- setting items and layouts
@@ -149,73 +162,72 @@ open class BoxView: UIView {
     }
     
     // inserting item after item with given view, if view is nil, then item inserted at index 0
-    public func insertItem(_ item: BoxItem, after view: UIView?, z: BoxLayout.ZPosition? = nil) {
+    public func insertItem(_ item: BoxItem, after object: BoxAnchorable?, z: BoxLayout.ZPosition? = nil) {
         isUpdatingItems = true
+
+        if let ind = itemIndexForObject(item.alObj) {
+            items.remove(at: ind)
+        }
+        var targetIndex = 0
+        if let index = items.firstIndex(where: {$0.alObj === object}) {
+            targetIndex = index + 1
+        }
+        items.insert(item, at: targetIndex)
+        
         if let itemView = item.view {
-            if managedViews.contains(itemView) {
-                if let ind = itemIndexForObject(itemView) {
-                    items.remove(at: ind)
-                }
-                itemView.removeFromSuperview()
+            if !managedViews.contains(itemView) {
+                configureManagedView(itemView)
+                managedViews.append(itemView)
             }
+            insertSubview(itemView, z: z ?? .above(object as? UIView))
         }
-        if let index = items.firstIndex(where: {$0.view == view}) {
-            items.insert(item, at: index + 1)
-        }
-        else {
-            items.insert(item, at: 0)
-        }
-        if let itemView = item.view {
-            itemView.translatesAutoresizingMaskIntoConstraints = false
-            if let ind = managedViews.firstIndex(where: {$0 == view}) {
-                managedViews.insert(itemView, at: ind + 1)
+        else if let itemGuide = item.guide {
+            if !managedGuides.contains(itemGuide) {
+                managedGuides.append(itemGuide)
             }
-            else {
-                managedViews.insert(itemView, at: 0)
-            }
-            insertSubview(itemView, z: z ?? .above(view))
+            addLayoutGuide(itemGuide)
         }
         isUpdatingItems = false
-        setNeedsUpdateConstraints()
+        setNeedsUpdateAllConstraints()
     }
     
     // inserting item before item with given view, if view is nil, then item inserted at the end
-    public func insertItem(_ item: BoxItem, before view: UIView?, z: BoxLayout.ZPosition? = nil) {
+    public func insertItem(_ item: BoxItem, before object: BoxAnchorable, z: BoxLayout.ZPosition? = nil) {
         isUpdatingItems = true
-        if let itemView = item.view {
-            if managedViews.contains(itemView) {
-                if let ind = itemIndexForObject(itemView) {
-                    items.remove(at: ind)
-                }
-                itemView.removeFromSuperview()
-            }
+        
+        if let ind = itemIndexForObject(item.alObj) {
+            items.remove(at: ind)
         }
-        if let index = items.firstIndex(where: {$0.view == view}) {
+        if let index = items.firstIndex(where: {$0.alObj === object}) {
             items.insert(item, at: index)
         }
         else {
             items.append(item)
         }
+        
         if let itemView = item.view {
-            itemView.translatesAutoresizingMaskIntoConstraints = false
-            if let ind = managedViews.firstIndex(where: {$0 == view}) {
-                managedViews.insert(itemView, at: ind)
-            }
-            else {
+            if !managedViews.contains(itemView) {
+                configureManagedView(itemView)
                 managedViews.append(itemView)
             }
-            insertSubview(itemView, z: z ?? .below(view))
+            insertSubview(itemView, z: z ?? .below(object as? UIView))
+        }
+        else if let itemGuide = item.guide {
+            if !managedGuides.contains(itemGuide) {
+                managedGuides.append(itemGuide)
+            }
+            addLayoutGuide(itemGuide)
         }
         isUpdatingItems = false
-        
-        setNeedsUpdateConstraints()
+        setNeedsUpdateAllConstraints()
     }
+
     
     // changing layout of existing item with specified object (view or guide)
     public func setLayout(_ layout: BoxLayout, for obj: BoxAnchorable?) {
         if let index = items.firstIndex(where: { $0.alObj === obj}) {
             items[index].layout = layout
-            setNeedsUpdateConstraints()
+            setNeedsUpdateAllConstraints()
         }
     }
     
@@ -259,14 +271,12 @@ open class BoxView: UIView {
     // MARK: -- overriden UIView functions and vars
     
     // When items are assigned or parameters affecting layout are changed,
-    // constraints are not changed immediatly. Only setNeedsUpdateConstraints() called.
+    // constraints are not changed immediatly. Only setNeedsUpdateAllConstraints() called.
     // Then updateConstraints() method is called automatically when boxView laying out own subviews.
     // You can call this method to force create constraints immediatly.
     override open func updateConstraints() {
         super.updateConstraints()
         if !isUpdatingItems {
-            NSLayoutConstraint.deactivate(managedConstraints)
-            managedConstraints = []
             addItemsConstraints()
             super.updateConstraints()
         }
@@ -292,7 +302,7 @@ open class BoxView: UIView {
                 if forceSubviewsSCA {
                     managedViews.forEach { setSCAIfNeed(for: $0) }
                 }
-                setNeedsUpdateConstraints()
+                setNeedsUpdateAllConstraints()
             }
         }
     }
@@ -323,6 +333,14 @@ open class BoxView: UIView {
     
 // MARK: - Internal -
     
+    // MARK: -- Managing items --
+    
+    internal var isUpdatingItems: Bool  = false
+    
+    internal func setNeedsUpdateAllConstraints() {
+        setNeedsUpdateConstraints()
+    }
+    
     internal func updateItems() {
         if isUpdatingItems { return }
         isUpdatingItems = true
@@ -331,29 +349,14 @@ open class BoxView: UIView {
         for (index, managedView) in managedViews.enumerated() {
             if !itemViews.contains(managedView) {
                 viewsToRemove.append(index)
-                if (excludeHiddenViews) {
-                    removeObserverForItemView(managedView)
-                }
-                managedView.removeFromSuperview()
             }
         }
         for index in viewsToRemove.reversed() {
-            managedViews.remove(at: index)
+            removeManagedView(at: index)
         }
         for view in itemViews {
             if !managedViews.contains(view) {
-                view.translatesAutoresizingMaskIntoConstraints = false
-                if forceSubviewsSCA {
-                    setSCAIfNeed(for: view)
-                }
-                managedViews.append(view)
-                addSubview(view)
-                if (excludeHiddenViews) {
-                    addObserverForItemView(view)
-                }
-            }
-            else {
-                bringSubviewToFront(view)
+                addManagedView(view)
             }
         }
         let itemGuides = items.compactMap{ $0.guide }
@@ -361,11 +364,10 @@ open class BoxView: UIView {
         for (index, managedGuide) in managedGuides.enumerated() {
             if !itemGuides.contains(managedGuide) {
                 guidesToRemove.append(index)
-                removeLayoutGuide(managedGuide)
             }
         }
         for index in guidesToRemove.reversed() {
-            managedGuides.remove(at: index)
+            removeManagedGuide(at: index)
         }
         for guide in itemGuides {
             if !managedGuides.contains(guide) {
@@ -373,11 +375,15 @@ open class BoxView: UIView {
                 addLayoutGuide(guide)
             }
         }
+        frontViews?.forEach { bringSubviewToFront($0) }
+        backViews?.forEach { sendSubviewToBack($0) }
         isUpdatingItems = false
-        setNeedsUpdateConstraints()
+        setNeedsUpdateAllConstraints()
     }
     
     internal func addItemsConstraints() {
+        NSLayoutConstraint.deactivate(managedConstraints)
+        managedConstraints = []
         let usedItems = (!excludeHiddenViews) ? items : items.compactMap { ($0.view?.isHidden ?? false) ? nil : $0 }
         createChainConstraints(boxItems: usedItems, axis: axis, spacing: spacing, insets: insets, constraints: &managedConstraints)
         items.createDimensions(constraints: &managedConstraints)
@@ -385,28 +391,59 @@ open class BoxView: UIView {
         createFlexDimentions(boxItems: usedItems, axis: axis, constraints: &managedConstraints)
         NSLayoutConstraint.activate(managedConstraints)
     }
-
     
-    internal var _insets: UIEdgeInsets = .zero {
-        didSet {
-            setNeedsUpdateConstraints()
-        }
-    }
-
-    internal var isUpdatingItems: Bool  = false
+    // MARK: -- Adding, removing and configuring item objects --
     
-    internal var observers = [Int: NSKeyValueObservation]()
-    
-    internal func addObserverForItemView(_ view: UIView) {
-        let intValue: Int = unsafeBitCast(view, to: Int.self)
-        observers[intValue] = view.observe(\.isHidden) { [unowned self] (_, _) in
-            self.setNeedsUpdateConstraints()
-        }
+    internal func addManagedView(_ view: UIView) {
+        configureManagedView(view)
+        addSubview(view)
+        managedViews.append(view)
     }
     
-    internal func removeObserverForItemView(_ view: UIView) {
-        let intValue: Int = unsafeBitCast(view, to: Int.self)
-        observers[intValue] = nil
+    internal func removeManagedView(at ind: Int) {
+        let view = managedViews[ind]
+        if (excludeHiddenViews) {
+            removeObserverForItemView(view)
+        }
+        view.removeFromSuperview()
+        managedViews.remove(at: ind)
+    }
+    
+    internal func removeManagedView(_ view: UIView) {
+        if let ind = managedViews.firstIndex(of: view) {
+            managedViews.remove(at: ind)
+        }
+        if (excludeHiddenViews) {
+            removeObserverForItemView(view)
+        }
+        view.removeFromSuperview()
+    }
+    
+    internal func addManagedGuide(_ guide: UILayoutGuide) {
+        managedGuides.append(guide)
+        addLayoutGuide(guide)
+    }
+    
+    internal func removeManagedGuide(at ind: Int) {
+        removeLayoutGuide(managedGuides[ind])
+        managedGuides.remove(at: ind)
+    }
+    
+    internal func removeManagedGuide(_ guide: UILayoutGuide) {
+        if let ind = managedGuides.firstIndex(of: guide) {
+            managedGuides.remove(at: ind)
+        }
+        removeLayoutGuide(guide)
+    }
+    
+    func configureManagedView(_ view: UIView) {
+        view.translatesAutoresizingMaskIntoConstraints = false
+        if forceSubviewsSCA {
+            setSCAIfNeed(for: view)
+        }
+        if (excludeHiddenViews) {
+            addObserverForItemView(view)
+        }
     }
     
     internal func setSCAIfNeed(for view: UIView) {
@@ -421,7 +458,29 @@ open class BoxView: UIView {
             }
         }
     }
+    
+    // MARK: -- Internal properties --
 
+    internal var _insets: UIEdgeInsets = .zero {
+        didSet {
+            setNeedsUpdateAllConstraints()
+        }
+    }
+
+    // MARK: -- Managing observers --
+    
+    internal var observers = [Int: NSKeyValueObservation]()
+    
+    internal func addObserverForItemView(_ view: UIView) {
+        let intValue: Int = unsafeBitCast(view, to: Int.self)
+        observers[intValue] = view.observe(\.isHidden) { [unowned self] (_, _) in
+            self.setNeedsUpdateAllConstraints()
+        }
+    }
+    
+    internal func removeObserverForItemView(_ view: UIView) {
+        let intValue: Int = unsafeBitCast(view, to: Int.self)
+        observers[intValue] = nil
+    }
 }
-
 
